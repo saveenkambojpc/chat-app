ba3e015571a253526f935a08458a78f7e27827ee
Saveen Kamboj <saveenkambojpc@gmail.com>
2022-08-15 13:55:05 +0530

    Initilaize firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDs6G3gWQyYV46rNxbiET5LpQoWi4OTgEI",
+    authDomain: "chat-web-app-980a1.firebaseapp.com",
+    databaseURL: "https://chat-web-app-980a1-default-rtdb.firebaseio.com",
+    projectId: "chat-web-app-980a1",
+    storageBucket: "chat-web-app-980a1.appspot.com",
+    messagingSenderId: "25307697221",
+    appId: "1:25307697221:web:628b20b46e1457289e532c"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+
