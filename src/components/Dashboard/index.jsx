9b901681467bab11da5e20592ad3f3600f9d46bb
Saveen Kamboj <saveenkambojpc@gmail.com>
2022-08-17 18:50:52 +0530

    Created Reuseable Editable Input

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-import { Button, Drawer, Icon } from 'rsuite'
+import { Button, Divider, Drawer, Icon } from 'rsuite'
 import { useProfile } from '../../context/profile.context'
+import EditableInput from '../EditableInput';
 
 const Dashboard = ({onSignOut}) => {
     const { profile } = useProfile();
+
+    const onSave = newData =>{
+        console.log("inside onSave",newData);
+    }
+    
     return (
         <>
             <Drawer.Header>
@@ -15,6 +21,8 @@ const Dashboard = ({onSignOut}) => {
             <Drawer.Body>
                 <h3>Hey,{profile.name} </h3>
                 <h3>Hey,{profile.email} </h3>
+                <Divider />
+                <EditableInput name="nickname" initialValue={profile.name} onSave={onSave} label={<h6 className='mb-2'>Nickname</h6>}/>
             </Drawer.Body>
 
             <Drawer.Footer>
---
+++ src/components/EditableInput.jsx
@@ -0,0 +1,51 @@
+import React, { useCallback, useState } from 'react'
+import { Alert, Icon, Input, InputGroup } from 'rsuite'
+
+const EditableInput = ({ name, initialValue, onSave, label = null, placeholder = "Write your value", emptyMsg = "Input is empty", ...inputProps }) => {
+
+    const [input, setInput] = useState(initialValue);
+    const [isEditable, setIsEditable] = useState(false);
+
+    const onInputChange = useCallback(value => {
+        setInput(value);
+    }, [])
+
+    const onSaveClick = async () => {
+        console.log("Inside onSaveClick");
+        const trimmed = input.trim();
+
+        if (trimmed === '') {
+            Alert.info("Empty Message", 4000);
+        }
+        if (trimmed !== initialValue) {
+            await onSave(trimmed)
+        }
+        setIsEditable(false);
+    }
+
+
+
+    const onEditClick = useCallback(() => {
+        setIsEditable(p => !p);
+        setInput(initialValue);
+    }, [initialValue])
+    return (
+        <div>
+            {label}
+            <InputGroup>
+                <Input {...inputProps} disabled={!isEditable} placeholder={placeholder} value={input} onChange={onInputChange} />
+                <InputGroup.Button onClick={onEditClick}>
+                    <Icon icon={isEditable ? 'close' : 'edit2'} />
+                </InputGroup.Button>
+
+                {isEditable &&
+                    <InputGroup.Button onClick={onSaveClick}>
+                        <Icon icon="check" />
+                    </InputGroup.Button>
+                }
+            </InputGroup>
+        </div>
+    )
+}
+
+export default EditableInput
